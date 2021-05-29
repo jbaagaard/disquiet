c136f4324dc06d7bcbabbb156c7159f1d7d1c54e
Jens Birkbak Aagaard <teamtunfiske@gmail.com>
2021-05-30 00:18:51 +0200

    test data

--- src/features/api/soundScapes.ts
+++ src/features/api/soundScapes.ts
@@ -2,5 +2,16 @@ import { Soundscape } from "../soundscapes/models";
 import { testRain, testRain2 } from "../data/soundscapes";
 
 export async function getSoundscapes(): Promise<Soundscape[]> {
-  return [testRain, testRain2];
+  return [
+    testRain,
+    testRain2,
+    testRain,
+    testRain2,
+    testRain,
+    testRain2,
+    testRain,
+    testRain2,
+    testRain,
+    testRain2,
+  ];
 }
