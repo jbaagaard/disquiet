714d1ca4638dc2c94f74768ad9cc0f0707f106f6
Jens Birkbak Aagaard <teamtunfiske@gmail.com>
2023-07-13 23:12:13 +0200

    styling

--- public/data/soundscapes.ts
+++ public/data/soundscapes.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7602827141a6e676b7092c1cbc036cad4c8f35a04539318bd42c69cf7ea5f3c
-size 1654
+oid sha256:3008835138058e36fb13689d0a0709ddf2b71395b92e45d5be6fa17b100a7c9e
+size 1205
